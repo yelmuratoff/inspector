b4cf1ab701cc424003b0664b225d76fd8674af37
Enguerrand_ARMINJON_MAC_2 <earminjon@sqli.com>
2025-09-26 14:04:13 +0200

    feat: Avoid massive unnecesary infinite rebuild logic

--- lib/src/widgets/inspector/overlay.dart
+++ lib/src/widgets/inspector/overlay.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import '../components/box_info_widget.dart';
 import 'box_info.dart';
 
-class InspectorOverlay extends StatefulWidget {
+class InspectorOverlay extends StatelessWidget {
   const InspectorOverlay({
     Key? key,
     required this.size,
@@ -17,51 +17,22 @@ class InspectorOverlay extends StatefulWidget {
   final BoxInfo? hoveredBoxInfo;
   final BoxInfo? comparedBoxInfo;
 
-  @override
-  _InspectorOverlayState createState() => _InspectorOverlayState();
-}
-
-class _InspectorOverlayState extends State<InspectorOverlay> {
-  final _panelVisibilityNotifier = ValueNotifier<bool>(false);
-
-  @override
-  void initState() {
-    super.initState();
-    _onTick(null);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  void _onTick(Duration? tick) {
-    if (!mounted) return;
-
-    setState(() {});
-
-    WidgetsBinding.instance.scheduleFrameCallback(
-      _onTick,
-      rescheduling: tick != null,
-    );
-  }
-
   bool _canRender(BoxInfo? boxInfo) =>
       boxInfo?.targetRenderBox.attached ?? false;
 
   @override
   Widget build(BuildContext context) {
-    if (!_canRender(widget.boxInfo) && !_canRender(widget.hoveredBoxInfo)) {
+    if (!_canRender(boxInfo) && !_canRender(hoveredBoxInfo)) {
       return const SizedBox.shrink();
     }
 
     return SizedBox(
-      width: widget.size.width,
-      height: widget.size.height,
+      width: size.width,
+      height: size.height,
       child: BoxInfoWidget(
-        boxInfo: widget.boxInfo,
-        hoveredBoxInfo: widget.hoveredBoxInfo,
-        comparedBoxInfo: widget.comparedBoxInfo,
+        boxInfo: boxInfo,
+        hoveredBoxInfo: hoveredBoxInfo,
+        comparedBoxInfo: comparedBoxInfo,
       ),
     );
   }
